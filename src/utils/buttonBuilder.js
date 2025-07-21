e919df4ea4452b7198693933e6a2cc33a3141617
Dennis <dennis-farris@hotmail.com>
2025-07-21 11:59:50 -0400

    added message specifying a country has no border countries if the border list is empty or missing

--- src/utils/buttonBuilder.js
+++ src/utils/buttonBuilder.js
@@ -1,21 +1,24 @@
 import { countryMap } from "../index.js";
 export default function buttonBuilder(buttonDiv, country) {
     var _a;
+    const fragment = document.createDocumentFragment();
     if (!country.borders || country.borders.length == 0) {
         const noBorders = document.createElement("p");
-        noBorders.textContent = "this country has no borders";
-        return;
+        noBorders.className = "info-no-border";
+        noBorders.textContent = "this country has no border countries";
+        fragment.appendChild(noBorders);
     }
-    const fragment = document.createDocumentFragment();
-    for (let border of country.borders) {
-        const button = document.createElement("button");
-        button.type = "button";
-        button.value = border;
-        button.className = "info-border-countries";
-        const countryName = (_a = countryMap.get(border)) === null || _a === void 0 ? void 0 : _a.name.common;
-        button.ariaLabel = `Border country: ${countryName}`;
-        button.textContent = countryName || "";
-        fragment.appendChild(button);
+    else {
+        for (let border of country.borders) {
+            const button = document.createElement("button");
+            button.type = "button";
+            button.value = border;
+            button.className = "info-border-countries";
+            const countryName = (_a = countryMap.get(border)) === null || _a === void 0 ? void 0 : _a.name.common;
+            button.ariaLabel = `Border country: ${countryName}`;
+            button.textContent = countryName || "";
+            fragment.appendChild(button);
+        }
     }
     buttonDiv.appendChild(fragment);
 }
--- src/utils/buttonBuilder.ts
+++ src/utils/buttonBuilder.ts
@@ -5,24 +5,25 @@ export default function buttonBuilder(
   buttonDiv: HTMLDivElement,
   country: Country
 ): void {
-  if (!country.borders || country.borders.length == 0)  {
-    const noBorders = document.createElement("p") as HTMLParagraphElement;
-    noBorders.textContent = "this country has no borders";
-    return;
-  }
   const fragment = document.createDocumentFragment();
 
-  for (let border of country.borders) {
-    const button = document.createElement("button") as HTMLButtonElement;
-    button.type = "button"
-    button.value = border;
-    button.className = "info-border-countries"
-    const countryName = countryMap.get(border)?.name.common
-    button.ariaLabel = `Border country: ${countryName}`
-    button.textContent = countryName|| "";
-    fragment.appendChild(button);
+  if (!country.borders || country.borders.length == 0) {
+    const noBorders = document.createElement("p") as HTMLParagraphElement;
+    noBorders.className = "info-no-border";
+    noBorders.textContent = "this country has no border countries";
+    fragment.appendChild(noBorders);
+  } else {
+    for (let border of country.borders) {
+      const button = document.createElement("button") as HTMLButtonElement;
+      button.type = "button";
+      button.value = border;
+      button.className = "info-border-countries";
+      const countryName = countryMap.get(border)?.name.common;
+      button.ariaLabel = `Border country: ${countryName}`;
+      button.textContent = countryName || "";
+      fragment.appendChild(button);
+    }
   }
 
   buttonDiv.appendChild(fragment);
-
 }
